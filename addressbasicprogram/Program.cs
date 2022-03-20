b6fa3db2a94ff4d977277a288aa83516c61e0cae
srikala <vlslaksh1899@gmail.com>
2022-03-20 20:34:41 +0530

    added UC-1

--- addressbasicprogram/Program.cs
+++ addressbasicprogram/Program.cs
@@ -6,11 +6,56 @@ using System.Threading.Tasks;
 
 namespace Address_Book
 {
-    class Program
+    class CreateContact
     {
+        string FirstName, LastName, Address, City, State, Email;
+        int ZipCode;
+        long PhoneNo;
+        public void Create_Contact()
+        {
+            Console.Write("Enter the First Name : ");
+            FirstName = Console.ReadLine();
+
+            Console.Write("Enter the Last Name : ");
+            LastName = Console.ReadLine();
+
+            Console.Write("Enter the Address : ");
+            Address = Console.ReadLine();
+
+            Console.Write("Enter the City : ");
+            City = Console.ReadLine();
+
+            Console.Write("Enter the State : ");
+            State = Console.ReadLine();
+
+            Console.Write("Enter the ZipCode : ");
+            ZipCode = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the Phone Number : ");
+            PhoneNo = Convert.ToInt64(Console.ReadLine());
+
+            Console.Write("Enter Your Email ID : ");
+            Email = Console.ReadLine();
+        }
+
+        public void DisplayContact()
+        {
+            Console.WriteLine("\n .................................... \n");
+            Console.WriteLine(" First Name : " + FirstName + "\n Last name : " + LastName + "\n Address : " + Address +
+                "\n City : " + City + "\n State : " + State + "\n ZipCode : " + ZipCode +
+                "\n Phone Number : " + PhoneNo + "\n Email ID : " + Email);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Address book Problem");
+            Console.WriteLine("Welcome to Address Book Program ");
+            CreateContact obj = new CreateContact();
+            obj.Create_Contact();
+            obj.DisplayContact();
+            Console.ReadLine();
         }
     }
 }
+
+
+
