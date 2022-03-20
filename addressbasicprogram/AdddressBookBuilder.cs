ad0260f2a9c9417f2922d1a21fcb1dca3bfa904b
srikala <vlslaksh1899@gmail.com>
2022-03-20 21:09:10 +0530

    added

---
+++ addressbasicprogram/AdddressBookBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Data
+{
+    internal class AdddressBookBuilder
+    {
+        internal readonly object list;
+
+        public AdddressBookBuilder()
+        {
+        }
+
+        internal void AddContact(Data contact)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Display_Contacts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- addressbasicprogram/Program.cs
+++ addressbasicprogram/Program.cs
@@ -3,59 +3,98 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
-namespace createContact
+namespace Data
 {
-    class CreateContact
+    class Program
     {
-        string FirstName, LastName, Address, City, State, Email;
-        int ZipCode;
-        long PhoneNo;
-        public void Create_Contact()
+        public void Create_Contacts(Data contact)
         {
             Console.Write("Enter the First Name : ");
-            FirstName = Console.ReadLine();
+            contact.First_Name = (Console.ReadLine());
 
             Console.Write("Enter the Last Name : ");
-            LastName = Console.ReadLine();
+            contact.Last_Name = (Console.ReadLine());
 
             Console.Write("Enter the Address : ");
-            Address = Console.ReadLine();
+            contact.Address = (Console.ReadLine());
 
             Console.Write("Enter the City : ");
-            City = Console.ReadLine();
+            contact.City = (Console.ReadLine());
 
             Console.Write("Enter the State : ");
-            State = Console.ReadLine();
+            contact.State = (Console.ReadLine());
 
-            Console.Write("Enter the ZipCode : ");
-            ZipCode = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Zip : ");
+            contact.Zipcode = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter the Phone Number : ");
-            PhoneNo = Convert.ToInt64(Console.ReadLine());
+            Console.Write("Enter the Phone No : ");
+            contact.Phone_No = Convert.ToInt64(Console.ReadLine());
 
-            Console.Write("Enter Your Email ID : ");
-            Email = Console.ReadLine();
+            Console.Write("Enter the Email Id : ");
+            contact.Email = (Console.ReadLine());
         }
-
-        public void DisplayContact()
-        {
-            Console.WriteLine("\n .................................... \n");
-            Console.WriteLine(" First Name : " + FirstName + "\n Last name : " + LastName + "\n Address : " + Address +
-                "\n City : " + City + "\n State : " + State + "\n ZipCode : " + ZipCode +
-                "\n Phone Number : " + PhoneNo + "\n Email ID : " + Email);
-        }
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program ");
-            CreateContact obj = new CreateContact();
-            obj.Create_Contact();
-            obj.DisplayContact();
-            CreateContact newContact = new CreateContact();
-            newContact.DisplayContact();
+            Console.WriteLine("Welcome to Address Book Program");
+            AdddressBookBuilder addressbook = new AdddressBookBuilder();
+            Program addressbookobj = new Program();
+            int choice = 1;
+            while (choice != 4)
+            {
+                Console.WriteLine("---------------------------------------------------------------------------------------------------");
+                Console.WriteLine("Menu : \n 1.Add Contact \n 2.Display Contact \n 3.Update Contact Using First Name \n 4.Exit ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("---------------------------------------------------------------------------------------------------");
+                switch (choice)
+                {
+                    case 1:
+                        DataSet contact1 = new DataSet();
+                        addressbookobj.Create_Contacts(contact1);
+                        addressbook.AddContact(contact1);
+                        Console.WriteLine("Contact Added Successfully.");
+                        break;
+                    case 2:
+                        addressbook.Display_Contacts();
+                        break;
+                    case 3:
+                        Console.WriteLine("which contact you want to update Enter First Name");
+                        String FirstName = (Console.ReadLine());
+                        int index =contact(FirstName);
+                        Console.WriteLine("Index is " + index);
+                        if (index == -1)
+                        {
+                            Console.WriteLine("Contact Not Found");
+                        }
+                        else
+                        { 
+                            DataView contact2 = new DataView();
+                            Console.WriteLine("---------------------------------------------------------------------------------------------------");
+                            Console.WriteLine("-----------Before Updating-----------");
+                            Console.WriteLine($"First Name :{addressbook.[index].First_Name} \nLast Name : {addressbook.[index].Last_Name} \nAddress : {addressbook.list[index].Address} \nCity : {addressbook.list[index].City} \nState : {addressbook.list[index].State} \nZip : {addressbook.list[index].Zip} \nPhone No : {addressbook.list[index].Phone_No} \nEmail : {addressbook.list[index].Email}");
+                            Console.WriteLine("---------------------------------------------------------------------------------------------------");
+                            addressbookobj.Create_Contacts(contact2);
+                            addressbook.[index] = contact2;
+                            Console.WriteLine("---------------------------------------------------------------------------------------------------");
+                            Console.WriteLine("-----------After Updating---------");
+                            Console.WriteLine($"First Name :{addressbook.[index].First_Name} \nLast Name : {addressbook.[index].Last_Name} \nAddress : {addressbook.list[index].Address} \nCity : {addressbook.list[index].City} \nState : {addressbook.list[index].State} \nZip : {addressbook.list[index].Zip} \nPhone No : {addressbook.list[index].Phone_No} \nEmail : {addressbook.list[index].Email}");
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("Program Exited");
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input");
+                        break;
+                }
 
-            Console.ReadLine();
+            }
+        }
+
+        private void Create_Contacts(DataView contact2)
+        {
+            throw new NotImplementedException();
         }
     }
 }
