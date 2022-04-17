4cd16eeffe8c250454a4a5b59c91c78685f8ad27
srikala <vlslaksh1899@gmail.com>
2022-04-17 15:55:23 +0530

    added new uc-11 case to the addressbook problem

---
+++ uc-11/addressbook.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Address_Book_System
+{
+    class AddressBook
+    {
+        private static List<PersonsDetails> contacts = new List<PersonsDetails>();
+
+        private static Dictionary<string, List<PersonsDetails>> addressBook = new Dictionary<string, List<PersonsDetails>>();
+
+        public static Dictionary<string, List<PersonsDetails>> cityBook = new Dictionary<string, List<PersonsDetails>>();
+        public static Dictionary<string, List<PersonsDetails>> stateBook = new Dictionary<string, List<PersonsDetails>>();
+
+        public static void AddTo(string name)
+        {
+            addressBook.Add(name, contacts);
+        }
+        public static void AddContact()
+        {
+            PersonsDetails person = new PersonsDetails();
+
+            Console.Write(" Enter your First name : ");
+            person.FirstName = Console.ReadLine();
+            Console.Write(" Enter your Last name : ");
+            person.LastName = Console.ReadLine();
+            Console.Write(" Enter your Address : ");
+            person.Address = Console.ReadLine();
+            Console.Write(" Enter your City : ");
+            person.City = Console.ReadLine();
+            Console.Write(" Enter your State : ");
+            person.State = Console.ReadLine();
+            Console.Write(" Enter your Zip Code : ");
+            person.ZipCode = Console.ReadLine();
+            Console.Write(" Enter your Phone Number : ");
+            person.PhoneNumber = Console.ReadLine();
+            Console.Write(" Enter your Email-ID : ");
+            person.Email = Console.ReadLine();
+
+            contacts.Add(person);
+            Console.WriteLine("\n {0}'s contact succesfully added", person.FirstName);
+        }
+
+        public static void Details()
+        {
+            if (contacts.Count == 0)
+            {
+                Console.WriteLine("Currently there are no people added in your address book.");
+            }
+            else
+            {
+                Console.WriteLine("Here is the list and details of all the contacts in your addressbook.");
+
+                foreach (var Detailing in contacts)
+                {
+                    Console.WriteLine("First name : " + Detailing.FirstName);
+                    Console.WriteLine("Last name : " + Detailing.LastName);
+                    Console.WriteLine("Address : " + Detailing.Address);
+                    Console.WriteLine("State : " + Detailing.State);
+                    Console.WriteLine("City : " + Detailing.City);
+                    Console.WriteLine("Zip Code : " + Detailing.ZipCode);
+                    Console.WriteLine("Phone number = " + Detailing.PhoneNumber);
+                }
+            }
+        }
+
+        public void SortByFirstName(List<PersonsDetails> person)
+        {
+            contacts = person.OrderBy(p => p.FirstName).ToList();
+        }
+    }
+}
