a82b6526ab3e10a830b23342aa71a7af9d7a6785
srikala <vlslaksh1899@gmail.com>
2022-04-20 15:05:01 +0530

    added uc-13 file of addressbook to the repositry

---
+++ uc-13/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uc_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- uc-14/Addressbook.cs
+++ uc-14/Addressbook.cs
@@ -1,8 +1,108 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 
-public class Class1
+namespace Address_Book_System
 {
-	public Class1()
-	{
-	}
+    class AddressBook
+    {
+        private static List<PersonsDetails> contacts = new List<PersonsDetails>();
+
+        private static Dictionary<string, List<PersonsDetails>> addressBook = new Dictionary<string, List<PersonsDetails>>();
+
+        public static Dictionary<string, List<PersonsDetails>> cityBook = new Dictionary<string, List<PersonsDetails>>();
+        public static Dictionary<string, List<PersonsDetails>> stateBook = new Dictionary<string, List<PersonsDetails>>();
+
+        public static void AddTo(string name)
+        {
+            addressBook.Add(name, contacts);
+        }
+        public static void AddContact()
+        {
+            PersonsDetails person = new PersonsDetails();
+
+            Console.Write(" Enter your First name : ");
+            person.FirstName = Console.ReadLine();
+            Console.Write(" Enter your Last name : ");
+            person.LastName = Console.ReadLine();
+            Console.Write(" Enter your Address : ");
+            person.Address = Console.ReadLine();
+            Console.Write(" Enter your City : ");
+            person.City = Console.ReadLine();
+            Console.Write(" Enter your State : ");
+            person.State = Console.ReadLine();
+            Console.Write(" Enter your Zip Code : ");
+            person.ZipCode = Console.ReadLine();
+            Console.Write(" Enter your Phone Number : ");
+            person.PhoneNumber = Console.ReadLine();
+            Console.Write(" Enter your Email-ID : ");
+            person.Email = Console.ReadLine();
+
+            contacts.Add(person);
+            Console.WriteLine("\n {0}'s contact succesfully added", person.FirstName);
+        }
+
+        public static void Details()
+        {
+            if (contacts.Count == 0)
+            {
+                Console.WriteLine("Currently there are no people added in your address book.");
+            }
+            else
+            {
+                Console.WriteLine("Here is the list and details of all the contacts in your addressbook.");
+
+                foreach (var Detailing in contacts)
+                {
+                    Console.WriteLine("First name : " + Detailing.FirstName);
+                    Console.WriteLine("Last name : " + Detailing.LastName);
+                    Console.WriteLine("Address : " + Detailing.Address);
+                    Console.WriteLine("State : " + Detailing.State);
+                    Console.WriteLine("City : " + Detailing.City);
+                    Console.WriteLine("Zip Code : " + Detailing.ZipCode);
+                    Console.WriteLine("Phone number = " + Detailing.PhoneNumber);
+                }
+            }
+        }
+
+        public static void CsvSerialise()
+        {
+            try
+            {
+                string csvPath = @"D:\Program\Address_Book_System\Address_Book_System\Files\CsvFile.csv";
+                var writer = File.AppendText(csvPath);
+
+
+                foreach (KeyValuePair<string, List<PersonsDetails>> item in addressBook)
+                {
+                    foreach (var items in item.Value)
+                    {
+                        writer.WriteLine(items.FirstName + ", " + items.LastName + ", " + items.PhoneNumber + ", " + items.Email + ", " + items.City + ", " + items.State + ", " + items.ZipCode + ".");
+
+                    }
+                    writer.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public static void CsvDeserialise()
+        {
+            string csvPath = @"D:\Program\Address_Book_System\Address_Book_System\Files\CsvFile.csv";
+            using (var reader = new StreamReader(csvPath))
+
+            {
+                string s = " ";
+                while ((s = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
 }
